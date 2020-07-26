0ae789b81fb7ea200bc053b7b1ff520dc5c25b92
jbatts82 <jbatts82@hotmail.com>
2020-07-25 22:41:02 -0400

    Adding comments and heater on off time

--- Heater.py
+++ Heater.py
@@ -1,6 +1,5 @@
 ###############################################################################
 # File Name  : Heater.py
-# Author     : John
 # Date       : 07/12/2020
 # Description: Heater Controls
 ###############################################################################
@@ -13,17 +12,27 @@ heater_cooldown_period = 10 #minutes
 process_period = 1 #minute
 needs_cool_down = False
 heater_state = False
+heater_on_time = 0
+heater_off_time = 0
 
 def init_heater():
     heater_off()
 
 def process_heater():
-    global heater_cooldown_period, process_period, needs_cool_down
+    global heater_cooldown_period, process_period, needs_cool_down, heater_on_time, heater_off_time
     print("Process_heater")
+    print("Heater On Time: ", heater_on_time, " Minutes")
+    print("Heater Off Time: ", heater_off_time, " Minutes")
     humidity = get_humidity()
     print(humidity)
     temp_f = get_temperature_f()
     print(temp_f)
+    
+    if(heater_state):
+        heater_on_time +=1
+    else:
+        heater_off_time +=1
+   
     if(temp_f < 83):
         if(humidity > 60):
             heater_on()
@@ -34,12 +43,6 @@ def process_heater():
     else:
         heater_off()
         print("Heater Off")
-
-def check_if_cool_down_needed(new_heater_state):
-    global heater_state
-    if heater_state != new_heater_state
-        if heater_state == True
-            needs_cool_down = True
         
 def heater_on():
     global heater_state
