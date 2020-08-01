82c09a7a9ea172ad89f2db04000faec0e640d5e9
jbatts82 <jbatts82@hotmail.com>
2020-08-01 18:03:50 -0400

    Adding light functionality
    led light

--- Light.py
+++ Light.py
@@ -4,20 +4,73 @@
 # Description : Controls TPLink Smart Plugs
 ###############################################################################
 
+
 import Plug
+from time import sleep
 
-def Turn_On_Light():
-    Plug.plug2_on()
+class Light:
+    def __init__(self):
+        self.name = "Mars Hydro LED x 2"
+        self.state = False
+        self.previous_s =  False
+        self.Process_Light()
+        
+    def Turn_On(self):
+        print("Light On")
+        self.state = Plug.plug2_on()
+        
+    def Turn_Off(self):
+        print("Light Off")
+        self.state = Plug.plug2_off()
     
-def Turn_Off_Light():
-    Plug.plug2_off()
-    
-def Get_Light_State():
-    light_state = Plug.get_plug2_state()
-    return light_state
+    def Get_Name(self):
+        return self.name
 
-def Print_Light_State():
-    if Get_Light_State():
-        print("Light is On")
-    else:
-        print("Light is Off")
+    def Get_State(self):
+        return self.state
+
+    def Process_Light(self):
+        print("Processing        :", self.name)
+        try: 
+            self.state = Plug.get_plug2_state()
+            self.previous_s = self.state
+        except: 
+            print("SIGNAL SNA        :", self.name)
+            self.state = self.previous_s
+        else:
+            print("Success Processing:", self.name)
+            self.previous_s = self.state
+        finally:
+            pass
+
+
+def light_test():
+    print("Light.py: Light Test")   
+    Plug.init_plug()
+    light = Light()
+    num = 0
+    light.Turn_On()
+    while True:
+        num = not num
+        if num:
+            light.Turn_On()
+        else:
+            light.Turn_Off()
+        light.Process_Light()
+        print("Light Name        : " , light.Get_Name())
+        print("Light State       : " , light.Get_State())
+        sleep(5)
+
+
+if __name__ == '__main__':
+    try:
+        light_test()
+    except:
+        print("System Error")
+    finally:
+        pass
+
+
+
+
+
