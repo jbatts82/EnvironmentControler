b63c0b059068ebe334f549fba83e7febc054e1f7
jbatts82 <jbatts82@hotmail.com>
2020-08-01 16:58:57 -0400

    refactoring relay interface
    update relay interaacfe

--- Songle.py
+++ Songle.py
@@ -7,33 +7,77 @@
 from gpiozero import LED
 from time import sleep
 
-relay1 = LED(14)
-relay2 = LED(18)
+relay1_config = {"name" : "Heater Switch", "gpio_zero_pin" : 14}
+relay2_config = {"name" : "Unassigned Switch", "gpio_zero_pin" : 18}
 
-def init_relay():
-    global relay1, relay2
-    relay1.off()
-    relay2.off()
-    
-def shut_down():
-    global relay1, relay2
-    print("Opening Relays")
-    relay1.off()
-    relay2.off()
+relay_array = {}
+relay_array[0] = relay1_config
+relay_array[1] = relay2_config
 
-def relay1_on():
-    global relay1
-    relay1.on()
-    
-def relay1_off():
-    global relay1
-    relay1.off()
 
-def relay2_on():
-    global relay2
-    relay2.on()
+class Relay:
+    def __init__(self, config):
+        self.name = config["name"]
+        self.relay = LED(config["gpio_zero_pin"])
+        self.relay.off()
+        self.state = False
+
+    def Get_Name(self):
+        return self.name
+        
+    def Turn_Off(self):
+        self.relay.off()
+        self.state = False
     
-def relay2_off():
-    global relay2
-    relay2.off()
+    def Turn_On(self):
+        self.relay.on()
+        self.state = True
+        
+    def Get_State(self):
+        return self.state
+        
+def get_relay_configs():
+    return relay_array
+
+def relay_test():
+    print("Songle.py: Relay Test")
     
+    relay_configs = get_relay_configs()
+    relay1 = Relay(relay_configs[0])
+    num = 0
+    
+    while True:
+        num = not num
+        print("Relay Name : ", relay1.Get_Name())
+        if num:
+            relay1.Turn_On()
+        else:
+            relay1.Turn_Off()
+        print("Relay State: ", relay1.Get_State())
+        sleep(5)
+            
+if __name__ == '__main__':
+    relay_test()       
+
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        
+        
+        
+        
+        
