4f40fb2ad61750b3f65101f83dba6c1c786aee1e
jbatts82 <jbatts82@hotmail.com>
2020-08-01 18:02:38 -0400

    adding defines
    clock interface

--- Time_Clock.py
+++ Time_Clock.py
@@ -6,6 +6,11 @@
 
 from datetime import datetime
 
+six_pm = "23:00"
+seven_pm = "24:00"
+eight_pm = "01:00"
+nine_pm = "02:00"
+
 def get_time():
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
