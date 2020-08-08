1f130d1fc99d628240334e1b1c14444afd3fb80b
jbatts82 <jbatts82@hotmail.com>
2020-08-08 14:25:12 -0400

    Updating Time Clock
    Also updated OS

--- Os.py
+++ Os.py
@@ -22,35 +22,37 @@ import Temperature
 
 def system_status():
     print("*******************************************************************************")
-    print("Date              :", Time_Clock.date_now())
-    print("Current Time      :", Time_Clock.time_now())
-    #print("Delta Time        :", Time_Clock.get_system_start_delta())
+    print("*******************************************************************************")
+    print("Date              :", time_clock.date_now())
+    print("Current Time      :", time_clock.time_now())
+    print("Delta Time        :", time_clock.get_time_since_start())
+    print("*******************************************************************************")
     print("Sensor            :", sensor1.get_sensor_name())
     print("Temp F            :", sensor1.get_temp_f())
     print("Humidity          :", sensor1.get_humidity())
-    
+    print("*******************************************************************************")
     print("Sensor            :", sensor2.get_sensor_name())
     print("Temp F            :", sensor2.get_temp_f())
     print("Humidity          :", sensor2.get_humidity())
-    
+    print("*******************************************************************************")
     print("Avg Temperature   :", the_temperature.get_average_temperature())
     print("Max Temperature   :", the_temperature.get_max_temperature())
     print("Min Temperature   :", the_temperature.get_min_temperature())
-
+    print("*******************************************************************************")
     print("Avg Humidity      :", the_humidity.get_average_humidity())
     print("Max Humidity      :", the_humidity.get_max_humidity())
     print("Min Humidity      :", the_humidity.get_min_humidity())
-    
+    print("*******************************************************************************")
     print("Fan Name          :", fan1.Get_Name())
     print("Fan State         :", fan1.Get_State())
-    
+    print("*******************************************************************************")
     print("Heater Name       :", heater.Get_Name())
     print("Heater State      :", heater.Get_State())
-    
+    print("*******************************************************************************")
     print("Light Name        :", light.Get_Name())
     print("Light State       :", light.Get_State())
     print("*******************************************************************************")
-
+    
 def Environment_Controller():
     print("Processing        : Environmental Controls")
     
@@ -76,7 +78,8 @@ def Task_60s():
     Environment_Controller()
 
    
-   
+#init OS Time Clock
+time_clock = Time_Clock.OS_Clock()
    
 #init wireless plugs
 Plug.init_plug()
@@ -111,7 +114,7 @@ schedule.every().minute.at(":15").do(fan1.Process_Fan)
 schedule.every().minute.at(":30").do(sensor2.process_sensor)
 schedule.every().minute.at(":45").do(light.Process_Light)
 
-Time_Clock.system_start_stamp()
+
 
 
 try:
--- Time_Clock.py
+++ Time_Clock.py
@@ -5,19 +5,39 @@
 ###############################################################################
 
 import datetime
+import time
 
-six_pm = "23:00"
-seven_pm = "24:00"
-eight_pm = "01:00"
-nine_pm = "02:00"
+class OS_Clock:
+    def __init__(self):
+        self.system_start_time = datetime.datetime.now()
+  
+    def get_time_since_start(self):
+        delta_time = datetime.datetime.now() - self.system_start_time
+        return delta_time
+  
+    def get_current_time_stamp(self):
+        now = datetime.datetime.now()
+        return now
+  
+    def date_now(self):
+        today = datetime.datetime.now().strftime("%Y-%m-%d")
+        today = str(today)
+        return(today)
 
+    def time_now(self):
+        now = datetime.datetime.now().strftime("%H:%M:%S")
+        now = str(now)
+        return(now)
     
-def date_now():
-    today = datetime.datetime.now().strftime("%Y-%m-%d")
-    today = str(today)
-    return(today)
-
-def time_now():
-    now = datetime.datetime.now().strftime("%H:%M:%S")
-    now = str(now)
-    return(now)
+    
+def time_clock_test():
+    print("Time_Clock.py: time_clock_test Test")
+    the_clock = OS_Clock()
+    
+    for x in range(9):
+        print("OS Delta Time: ", the_clock.get_time_since_start())
+        print("OS Time      : ", the_clock.get_current_time_stamp())
+        time.sleep(1)
+         
+if __name__ == '__main__':
+    time_clock_test() 
