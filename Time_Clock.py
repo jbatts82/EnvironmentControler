a72c643dfb1a6f80226230c0fdfb5f488f6223e6
jbatts82 <jbatts82@hotmail.com>
2020-08-07 23:02:14 -0400

    Refactor

--- Time_Clock.py
+++ Time_Clock.py
@@ -11,6 +11,7 @@ seven_pm = "24:00"
 eight_pm = "01:00"
 nine_pm = "02:00"
 
+global system_start_time
     
 def date_now():
     today = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -21,3 +22,14 @@ def time_now():
     now = datetime.datetime.now().strftime("%H:%M:%S")
     now = str(now)
     return(now)
+    
+    
+def system_start_stamp():
+    system_start_time = time_now()
+
+def get_system_start_delta():
+    return time_now() - system_start_time
+
+
+    
+    
