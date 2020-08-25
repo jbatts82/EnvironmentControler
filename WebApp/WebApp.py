29ac7d6d17bf99001c87712c0928fe808179f84e
jbatts82 <jbatts82@hotmail.com>
2020-08-24 23:09:25 -0400

    Added Files

---
+++ WebApp/WebApp.py
@@ -0,0 +1,47 @@
+###############################################################################
+# File Name  : TheWebServer.py
+# Date       : 07/11/2020
+# Description: Displays sensor output to web page.
+###############################################################################
+
+import datetime
+import sys
+sys.path.append('/home/mario/EnvironmentController/SupportFiles/')
+from DB_Handler import DB_Manager
+sys.path.append('/home/mario/EnvironmentController/Sensor/')
+import DHT11
+from flask import Flask, render_template
+
+database_location = '/home/mario/EnvironmentController/readings.db'
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    last_row = sensor1_db.get_last_row('*')
+    timeString = last_row[0]
+    temp_1 = last_row[1]
+    humidity = last_row[2]
+    templateData = {
+      'title' : 'Temperature: ',
+      'time': timeString,
+      'temp_1' : temp_1,
+      'hum' : humidity,
+    }
+      
+    return render_template('index.html', **templateData)
+
+
+
+if __name__ == '__main__':
+    print("Starting File: ", __file__)
+    sensor1_name = DHT11.sensor1_config['name']
+    sensor2_name = DHT11.sensor2_config['name']
+    sensor1_db = DB_Manager(database_location, sensor1_name)
+    sensor2_db = DB_Manager(database_location, sensor2_name)
+    #print(sensor1_db.get_last_row("*"))
+    app.run(debug=True, host='0.0.0.0')
+    
+    
+    
+    
+    
